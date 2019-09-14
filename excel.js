74ad3ad7bb222a06feb7f1821a29842d0e6580df
georgia-pap <gpapavgeri@outlook.com>
2019-09-14 16:55:59 +0300

    Add event listener in excel column

--- excel.js
+++ excel.js
@@ -5,12 +5,44 @@ console.log("Running");
 
 let unitCol = document.querySelector("#units");
 console.log(unitCol);
+/*
 // Change style:
 unitCol.style.backgroundColor = "red";
 unitCol.style.border = "1px solid black";
 
 // Change context:
 unitCol.textContent = "55555555";
+*/
+
+// HTMLElement.addEventListener(EVENT, CALLBACK);
+// unitCol.addEventListener("click", handleClick);
+unitCol.addEventListener("input", handleInput);
+
+// Hoisting - Μεταφέρει τις δηλώσεις των function στην αρχή του scope (εδώ page) - Μέσα σε function -> στην αρχή της function
+// function handleClick(e) {
+//     console.log(1, e);
+//     console.log(2, e.target);
+//     console.log(3, e.target.textContent);
+//     e.target.textContent = "WOW";
+// }
+function handleInput(e) {
+    // G = E * D (Columns)
+    // E: 
+    let unitsProjected = e.target; 
+    // G:
+    let G = unitsProjected.nextElementSibling.nextElementSibling;
+    // D:
+    let D = unitsProjected.previousElementSibling;
+
+    let valE = unitsProjected.textContent;
+    let valD = D.textContent;
+    valE = parseFloat(valE);
+    valD = parseInt(valD);
+
+    console.log(typeof valE, typeof valD);
+    console.log("G = ", (valE * valD));
+    G.textContent = valE * valD;
+}
 
 // run();
 try {
@@ -20,3 +52,5 @@ try {
 } catch (e) {
     console.log(e, e.message);
 }
+
+console.log("run code");
